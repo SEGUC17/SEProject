55950a6d13543e1a4920465a59b0a7adee51e21a
habibahshm <habibahshm@users.noreply.github.com>
2017-04-01 19:22:58 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ var cookieParser = require('cookie-parser');
 var multer = require('multer');
 var upload = multer({ dest: './uploads' });
 
+var SPController = require('./controllers/ServiceProviderController');
+var ServiceProvider = require('./db/ServiceProvider');
+
 var app = express();
 
 mongoose.connect('mongodb://localhost/platform');
@@ -30,6 +33,15 @@ app.use(passport.session());
 
 app.use(router);
 
+SPController.addSP(new ServiceProvider({"organizationName":"asasas",
+	"field" :"wdwdwqdqwdwdw",
+	"description" :"swqqswswqsw",
+	"mobileNumber" :123456,
+	"email" :"ssts@gmail.com",
+	"address" :"ssswswswswsws",
+	"polices":"	qsqsqsqsqss"}));
+
+console.log(ServiceProvider.find());
 
 
 // start the server

7283ced19487f50177be4759e0f4ee0c5bc6dfaf
habibahshm <habibahshm@users.noreply.github.com>
2017-04-01 17:48:21 +0200

    Update ServiceProvider.js

--- src/db/ServiceProvider.js
+++ src/db/ServiceProvider.js
@@ -1,66 +1,60 @@
-
-var mongoose=require('mongoose');
-//var bcrypt   = require('bcrypt-nodejs');
-var Course=require('./Courses');
-
-require('mongoose-type-email');
-
-var ServiceProviderSchema=mongoose.Schema({
-
-	username:{
-		type:String
-		// required:true
-	},
-	expirationDate:{
-		type:Date
-	},
-	passowrd:{
-		type:String
-	},
-	organizationName:{
-		type:String,
-		unique:true
-		// required:true
-	},
-	field :{
-		type:String,
-		required:true
-	},
-	description :{
-		type:String,
-		required:true
-	},
-	mobileNumber :{
-		type:Number,
-		required:true,
-		unique:true
-	},
-	email :{
-		type: mongoose.SchemaTypes.Email, 
-		required: true, 
-		unique:true
-	},
-	address :{
-		type:String,
-		required:true
-	},
-	polices:{
-		type:String,
-		required:true
-	},
-	logo :{
-		type:String,
-
-	},
-	listOfCourses:
-
-		[{type:Schema.Types.ObjectId ,ref:'Course'}]
-	
-
-});
- 
-var ServiceProvider = module.exports = mongoose.model('ServiceProvider', ServiceProviderSchema);
-
-
-    
-
+    var mongoose=require('mongoose');
+    //var bcrypt   = require('bcrypt-nodejs');
+    var Course=require('./Courses');
+     
+     
+    var ServiceProviderSchema=mongoose.Schema({
+     
+    	username:{
+    		type:String
+    		// required:true
+    	},
+    	expirationDate:{
+    		type:Date
+    	},
+    	passowrd:{
+    		type:String
+    	},
+    	organizationName:{
+    		type:String,
+    		unique:true
+    		// required:true
+    	},
+    	field :{
+    		type:String,
+    		required:true
+    	},
+    	description :{
+    		type:String,
+    		required:true
+    	},
+    	mobileNumber :{
+    		type:Number,
+    		required:true,
+    		unique:true
+    	},
+    	email :{
+    		type:String,
+    		required:true,
+    		unique:true
+    	},
+    	address :{
+    		type:String,
+    		required:true
+    	},
+    	polices:{
+    		type:String,
+    		required:true
+    	},
+    	logo :{
+    		type:String,
+     
+    	},
+    	listOfCourses:
+     
+    		[{type:String}] //this string is an ID of a course, and you can get the course using .findbyID()
+     
+     
+    });
+     
+    var ServiceProvider= module.exports=mongoose.model('ServiceProvider',ServiceProviderSchema)

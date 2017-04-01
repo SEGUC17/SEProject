106d95a51288553fcc417856f5abd82232803a25
habibahshm <habibahshm@users.noreply.github.com>
2017-04-01 19:32:57 +0200

    Update Reviews.js

--- src/db/Reviews.js
+++ src/db/Reviews.js
@@ -1,66 +1,32 @@
 var mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/platform");
-
-var db = mongoose.connection;
-
- 
-
-db.on("error", console.error.bind(console, "connection error"));
-
-db.once("open", function(callback) {
-
-    console.log("Connection succeeded.");
-
-   });
-
 var Schema = mongoose.Schema;
 
 
 
 
 
-var Reviews = new Schema({
-
-
-
-  userName:{type: mongoose.Schema.Types.ObjectId, ref: 'Student'},
-
-  review:{type:String},
-
+var Reviews = mongoose.Schema({
+ 
+// holds the student ID who has written the review
+  StudentID: {
+  	type: String
+  },
+ 
+  review: {
+  	type: String
+  },
+ 
+// to determine whether the review is +ve or -ve
   isNeg:Boolean,
-
-  courses:[{type:mongoose.Schema.Types.ObjectId, ref: 'Course'}]
-
-
-
+ 
+// holds the course ID which has been reviewed
+  courseID: {
+  	type: String
+  }
+ 
 })
 
 
-
-Reviews.methods.add = function(){
-
-	console.log("enteredd");
-
-	//var rev = new review({"review": "Hell", "isNeg":true, courses:["58de7687f72024611ebaad7f"]});
-
-	this.save(function(err){
-
-		console.log("saved");
-
-		if(err){
-
-
-
-			console.log(err);
-
-		}
-
-	})
-
-	console.log("done");
-
-}
 
 var review  = mongoose.model("Reviews", Reviews);
 

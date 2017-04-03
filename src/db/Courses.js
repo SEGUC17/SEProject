f6ac30a7eb93bb583563fab4688b99a8f8403a94
habibahshm <habibahshm@users.noreply.github.com>
2017-04-03 22:07:27 +0200

    Update Courses.js

--- src/db/Courses.js
+++ src/db/Courses.js
@@ -6,75 +6,91 @@ var Student = require('./Student');
 var Schema = mongoose.Schema;
 
 var Courses = mongoose.Schema({
- 
-    title: { 
+
+    title: {
         type: String,
         required: true,
-        unique: true 
+        unique: true
     },
- 
+
     centerName: {
         type:String,
         required:true
     },
- 
+
     centerLocation: {
         type:String,
         required:true
     },
- 
+
     type: {
         type:String,
         required:true
     },
- 
+
     description: {
         type:String,
         required:true
     },
- 
+
     startDate: {
         type:Date,
         required:true
     },
- 
+
     endDate: {
         type:Date,
         required:true
     },
- 
+
     capacity:{
         type:Number,
-        required:true 
+        required:true
     },
- 
+
     fees:{
         type:String,
         required:true
     },
- 
+
 // holds the announcements of this course
     announcements: [{
         type:String
     }],
- 
+
 // holds all the IDs of students that are taking this course
     enrolledStudentsIDs : [{
         type: String
     }],
- 
+
 // holds the ID of the service provider that provides this course
     serviceProviderID:{
-        type:String
+        type:String,
+        default:0
     },
- 
+
 // hold the IDs of the reviews of this course
     ReviewsIDs: [{
         type: String
-    }]
- 
- 
+    }],
+
+ countNeg:{
+    type: Number,
+    default:0
+
+ },
+ totalCount:{
+   type: Number,
+   default: 0
+ }
+
 })
+
+
+module.exports.updateCourse = (title, course,options, callback) => {
+    var query = {title:title};
+    Courses.update(query, course, options,callback);
+}
 
 var Courses = mongoose.model("Course", Courses);
 

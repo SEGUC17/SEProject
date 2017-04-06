69121cd5f4f6fa79b7af5641482c550d67fb12ae
habibahshm <habibahshm@users.noreply.github.com>
2017-04-06 21:06:44 +0200

    Update server.js

---
+++ src/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = require('./routes');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var path = require('path');
+var session = require('express-session');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+
+
+var Admin = require('./db/Admin');
+var AdminController = require('./controllers/AdminController');
+
+var ServiceProviderController = require('./controllers/ServiceProviderController');
+var StudentController = require('./controllers/StudentController');
+var ServiceProvider =require('./db/ServiceProvider');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/platform');
+
+// configure app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true,
+}));
+
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+app.use(passport.initialize());  
+app.use(passport.session()); 
+
+app.use(router);
+
+ServiceProvider.loop();
+
+
+
+// start the server
+app.listen(8080, function(){
+    console.log("server is listening on port 8080");
+});
+

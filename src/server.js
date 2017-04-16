75d633a8871dfed1f0153efa4bd257624a2a6845
habibahshm <habibahshm@gmail.com>
2017-04-16 11:57:24 +0200

    removed unwanted depedencies

--- src/server.js
+++ src/server.js
@@ -3,13 +3,11 @@ var router = require('./routes');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
-var session = require('express-session');
-var passport = require('passport');
 var flash    = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var multer = require('multer');
 var upload = multer({ dest: './uploads' });
-var Cookies = require( "cookies" );
+
 
 var jwt = require('jsonwebtoken');
 
@@ -36,18 +34,9 @@ app.set('super-secret', 'sedki');
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 
-app.use(session({secret: "secret", resave: false, saveUninitialized: false}));
 
 app.use(router);
 
-// console.log("hhhhhh");
-
-
- // var sp = new ServiceProvider ({username: "youmna",
- //                               password: "123"
- // });
- // AdminController.verifySP();
- // ServiceProviderController.SP(sp);
 
 // start the server
 app.listen(8080, function(){

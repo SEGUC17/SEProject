a485ba0fb15422f3894152207d882f273d3d0b8f
hebaelshawarby <heba2k@gmail.com>
2017-04-18 17:13:22 +0200

    service provider register, service provider login and part of the login token

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -1,17 +1,61 @@
+angular.module('MainController', ['indexSrv'])
+.controller('MainController',function($scope,indexSrv,$location) {
+//console.log('main')
+
+indexSrv.getVerifiedServiceProvider().then(function(res){
+	$scope.title=res.data
+})
 
 
-myApp.controller('MainController', function($scope,indexSrv,$location) {
 
-  $scope.title=  "youmnaaa" ;
-  // indexSrv.getVerifiedServiceProvider();
+	if(indexSrv.IsLoggedIn()){
+		console.log("user logged in")
+		indexSrv.GetCurrentUser().then(function(response){
+			console.log(response);
+		});
+	}else
+	{
+	console.log("user not  logged in")
+
+	}
+
+
+	this.login=function(data){
+		indexSrv.ServiceProviderLogin(this.data).then(function(response){
+		//	console.log(response.data)
+			//console.log("the token is: "+response.data.token)
+
+			if(response.data.success==true)
+			$location.path('/welcome')
+
+		})
+	};
+
+	this.logout=function(){
+		indexSrv.LogOut();
+		console.log("log")
+	   // $location.path('/')
+
+
+	}
+
+
+
+
+  //
   // indexSrv.getCatalogPage();
-$location.url('/welcome');
-$scope.search = function() {
+//$location.url('/welcome');
+// $scope.search = function() {
        
-        $location.url('/search');
+//         $location.url('/search');
         
-    };
+//     };
 
+
+// indexSrv.spRegister($scope.sp).then(function)(res){
+//         $location.url('/register');
+
+// }
 
 
   
--- app/starter.js
+++ app/starter.js
@@ -1,7 +1,7 @@
 
 // console.log("appp")
 
-myApp = angular.module('myApp', ['ui.bootstrap','ngRoute']);
+var myApp = angular.module('myApp', ['ui.bootstrap','ngRoute','MainController','indexSrv','spctr','businessServ','spAuthServ']);
 
 
 myApp.config(function($routeProvider) {
@@ -9,9 +9,9 @@ myApp.config(function($routeProvider) {
 
   // route for the landingPage page
     .when('/', {
-    templateUrl: '/index.html',
-    controller: 'MainController',
-    controllerAs: 'MainController'
+    templateUrl: 'views/welcome.html',
+    // controller: 'MainController',
+    // controllerAs: 'MainController'
                })
     .when('/catalog',{
 
@@ -30,6 +30,50 @@ myApp.config(function($routeProvider) {
    
 
     })
+   .when('/registeredSP',{
+
+    templateUrl:'views/registeredSp.html',
+      controller: 'MainController',
+    controllerAs: 'MainController'
+
+    })
+   .when('/register',{
+
+    templateUrl:'views/register.html',
+      controller: 'spCon',
+    controllerAs: 'spCon'
+
+    })
+   .when('/test',{
+
+    templateUrl:'views/test.html'
+    
+
+    })
+    .when('/login',{
+
+    templateUrl:'views/login.html',
+      controller: 'MainController',
+    controllerAs: 'MainController'
+
+    
+
+    })
+    .when('/logout',{
+
+    templateUrl:'views/logout.html',
+      controller: 'MainController',
+    controllerAs: 'MainController'
+
+    
+
+    })
+    .otherwise({
+    redirectTo: "/"
+  });
+
+    
+  
 
 });
 
@@ -39,7 +83,7 @@ myApp.config(function($routeProvider) {
 //   $scope.title='Top Sellers in Books' ;
   
 // }])
-	
+  
 
 // angular.module('myApp', []).controller('myAppCtrl', function($scope) {
 

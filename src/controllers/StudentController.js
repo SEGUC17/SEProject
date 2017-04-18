d2e6c5a8e5e2ad79a8d052e32e087f35fa788b70
Fatma Elazab <fatmaelazab@gmail.com>
2017-04-19 00:34:11 +0200

    fixed type review

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -137,22 +137,35 @@ let StudentController = {
     },
 
 //getStudentProfile function displays for the student his username,profile pictures and his list of courses
+     //var courses =[];
+   getStudentProfile : function(req,res,cb) {
 
-   getStudentProfile : function(req,res) {
-    var array=[];
-  Student.findOne({username:req.session.username}).lean().exec(function(err,student){
-    if (err) throw err
- else {
-         for(var i=0;i<student.ListOfCourses.length;i++){
-           Course.findById(student.ListOfCourses[i]._id,function(err,course){
-             array = array.concat([course.title]);
-           })
-         }
-      }
-  });
+	var indx=2;
+  Student.findOne({username:req.decoded.username}).lean().exec(function(err,student){
+	 for(var i=0; i<student.ListOfCourses.length;i++){
 
-},
+		 Course.findById(student.ListOfCourses[i],function(err,course){
+			 //console.log(course);
 
+				 //glo.push(course.title);
+					glo[indx] = course.title;
+						indx++;
+
+
+			});
+
+
+}
+glo[0]=student.username;
+glo[1]=student.profilePicture;
+
+
+	cb(err,glo,"SUCCESS");
+	for(var w=glo.length-1;w>=0;w--){
+	glo.pop();}
+
+		     });
+			 },
 
 // search function can make the student or the visitor search for a specific course by its title,type,center name,or center location 
   search:function(req,res){
@@ -186,84 +199,113 @@ let StudentController = {
 },
 
 // typeReview function makes the student able to write a review for a course that he took
-typeReview: function(req,res){
-var flag =0;
-Student.findById(req.sesssion._id,function(err,student){
- if(err) throw err;
-  for(var i=0;i< student.ListOfCourses.length;i++){
-    Course.findOne({title:req.body.courseTitle},function(err,coursetitle){
+
+typeReview: function(req,res,cb){
+	var w=[];
+  Student.findById(req.decoded.id,function(err,student){
+   if(err)
+	 cb(err,"ERROR","ERROR");
+	 Course.findOne({title:req.body.courseTitle},function(err,c){
+		 if(err)
+		 cb(err,"ERROR","ERROR");
+
+ var courseID=c.id;
+var f=0;
+
+    for(var i=0;i< student.ListOfCourses.length;i++){
+		if(student.ListOfCourses[i] == courseID)
+		{
+			f=1;
+		  Course.findById(student.ListOfCourses[i],function(err,course){
+		  var newReview = new Review({
+		    StudentID:req.decoded.id,
+		    review:req.body.review,
+		    isNeg:req.body.isNeg,
+		    courseID:courseID
+		  });
+		  newReview.save(function(err,newReview){
+		    if(err)
+				cb(err,"ERROR","ERROR");
 
 
-  if(student.ListOfCourses[i] == coursetitle)
-  {
-    flag=1;
-    Course.findById(student.ListOfCourses[i]._id,function(err,course){
-    var newReview = new Review({
-      StudentID:req.session._id,
-      review:req.body.review,
-      isNeg:req.body.isNeg,
-      courseID:coursetitle._id
-    });
-    newReview.save(function(err,newReview){
-      if(err) throw err;
-      req.flash('success_msg','Your review is added');
-    });
-    var courseTitle = course.title ;
-   var array = course.ReviewsIDs.concat([newReview._id]);
-    course.ReviewsIDs= array;
-    if(req.body.isNeg==1){
-    course.countNeg=course.countNeg+1;
-    course.totalCount=course.totalCount+1;
-    course.save(function(err,course){
-      if(err) throw err;
-    });
-
-  if((course.countNeg/course.totalCount)>=0.5){
-    ServiceProvider.findById(course.serviceProviderID,function(err,SP){
-
-      var item = {
-        typeOfNotification:"BAD REVIEWS ON **"+course.title
-      }
-      ServiceProvider.update(SP,{"$addToSet":{listOfNotification:item}} ,function(err,res){
-        if(err)throw err;
-      })
-
-      SP.save(function(err,SP){
-        if(err) throw err;
-      });
+		  });
 
 
-            var item2={
-            typeOfNotification:"BAD REVIEWS on " +course.title,
-            ServiceProviderUsername:SP.username
-            }
+var indx2=0;
 
-            Admin.findOne({username:"Admin"},function(err,resx){
-
-              Admin.update(resx,{$push:{ listOfNotification :item2}},{safe:true,upsert:true},function(err,ress) {
-              if(err) throw err;
+			for(var i=0;i<course.ReviewsIDs.length;i++){
+		 	 Review.findById(course.ReviewsIDs[i],function(err,rouu){
+		 		 glo2[indx2]=rouu;
+		 	 });
+		  }
 
 
-            resx.save(function(err,resx){
-              if(err) throw err;
-            });   });
+
+			cb(err,glo2,"SUCCESS");
+			for(var w=glo2.length-1;w>=0;w--){
+			glo.pop();}
+
+		  var courseTitle = course.title ;
+		 var array = course.ReviewsIDs.concat([newReview.id]);
+		  course.ReviewsIDs= array;
+		  if(req.body.isNeg==1){
+		  course.countNeg=course.countNeg+1;
+		  course.totalCount=course.totalCount+1;
+		  course.save(function(err,course){
+		    if(err) cb(err,"ERROR","ERROR");
+		  });
+
+		if((course.countNeg/course.totalCount)>=0.5){
+		  ServiceProvider.findById(course.serviceProviderID,function(err,SP){
+		   // SP.listOfNotification=SP.listOfNotification.concat(course.title+" has exceeded the maximum number of negative reviews");
+        var item = {
+					typeOfNotification:"BAD REVIEWS ON **"+course.title
+				}
+				ServiceProvider.update(SP,{"$addToSet":{listOfNotification:item}} ,function(err,res){
+					if(err)
+				cb(err,"ERROR","ERROR");
+				})
+
+
+//console.log(SP.listOfNotification.typeOfNotification);
+				SP.save(function(err,SP){
+		      if(err) cb(err,"ERROR","ERROR");
+		    });
+
+
+							var item2={
+							typeOfNotification:"BAD REVIEWS on " +course.title,
+							ServiceProviderUsername:SP.username
+							}
+
+							Admin.findOne({username:"Admin"},function(err,resx){
+
+								Admin.update(resx,{$push:{ listOfNotification :item2}},{safe:true,upsert:true},function(err,ress) {
+								if(err)
+							cb(err,"ERROR","ERROR");
+
+							resx.save(function(err,resx){
+								if(err) cb(err,"ERROR","ERROR");
+							});		});
 });
 
 
-    });
+		  });
 
+		 }
    }
-   }
-
-  });
+	  });
 }
-});
- }
- if(flag==0){
-   req.flash('error_msg','You Can not review this course');
- }
-});
-},
+}
+if(f==0)
+cb(err,"You can't review this course","ERROR");
+		});
+
+	});
+
+
+
+ },
 
    //the student view all the courses he is enrolled to 
        viewStudentListOfCourses : function(req, res){
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 var Course=require('./db/Courses');
@@ -6,99 +7,45 @@ var Admin=require('./db/Admin');
 
 var StudentController = require('./controllers/StudentController');
 var ServiceProviderController = require('./controllers/ServiceProviderController');
-var AdminController = require('./controllers/AdminController');
+var app = require('./server.js')
 
-var app = require('./server.js');
+router.post('/forbussinus/login', function(req,res){
 
-var path=require('path');
+	ServiceProviderController.SPLogin(req,res,function(sp, error){
+
+//check if match username pwd 
+	var token = app.jwt.sign({username: sp.username, id: sp._id, orgName: sp.organizationName, type:"ServiceProvider"}, app.app.get('super-secret'), {
+          //expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+	res.json({
+		success:true,
+		token :token
 
 
-router.get('/',function (req,res){
-        res.sendFile(path.join(__dirname,'../','app','index.html'))
+	})
+
+})
 });
-
-    router.post('/forbussinus/login', function(req,res){
-    	ServiceProviderController.SPLogin(req,res,function(sp, error){
-     
-    if(!error){
-    //check if match username pwd 
-    	var token = app.jwt.sign(
-    		{username: sp.username,
-    		 id: sp._id,
-    		  type:"ServiceProvider"}, 
-    		  app.app.get('super-secret'), {
-     
-            });
-     
-    	return res.json({
-    		success:true,
-    		token :token
-     
-    	})
-    } else{
-    	return res.json({
-    		success:false,
-    		message:"wrong username or password"
-       });
-     
-       }
-     });
-    });
 //login bta3 el student
 router.post('/login', function(req,res){
+
 	StudentController.checkStudentLogin(req,res,function(student, error){
-      if(error){
-		return res.json({
-			success: false,
-			message:"wrong username or password"
-		});
-	}
 
 //check if match username pwd 
 	var token = app.jwt.sign({username: student.username, id: student._id, type:"Student"}, app.app.get('super-secret'), {
           //expiresInMinutes: 1440 // expires in 24 hours
         });
 
-	return res.json({
+	res.json({
 		success:true,
 		token :token
+
 
 	})
 
 })
 });
-
-router.post('/register', function(req,res){
-
-	StudentController.studentSignUP(req,res,function(student, error){
-
-//check if match username pwd 
-	var token = app.jwt.sign({username: student.username, id: student._id, type:"Student"}, app.app.get('super-secret'), {
-          //expiresInMinutes: 1440 // expires in 24 hours
-        });
-
-	return res.json({
-		success:true,
-		token :token
-
-	})
-
-})
-});
-
- router.post('/serviceprovider/register',function(req,res){
-    	return ServiceProviderController.spRegister(req,res);
-    });
-
-
-     router.get('/home/viewreg',function(req,res){
-      
-     ServiceProviderController.getAllVerifiedServiceProvider(req,res, function(err, sp){
-          res.send(sp);
-     });
-    });
- 
-
 
 router.use(function(req,res,next){ //this middleware adds the decoded token the req before continuing to any other routes
                                    //so if you need to access an attribute saved in the token,
@@ -131,41 +78,101 @@ router.use(function(req,res,next){ //this middleware adds the decoded token the 
 	}
 })
 
-  router.post('/adminhomepage/verify', function(req,res){
-       return AdminController.verifySP(req,res);
-    });
+router.post('/studentprofile',function(req,res){
+  if(req.decoded.type=="Student"){
+StudentController.getStudentProfile(req,res,(err,prof,type)=>{
+
+  res.json({
+ type:type,
+ message:"STUDENT PROFILE RETREIVED",
+ content:prof
+  });
+  });
+
+}else{
+res.json({
+type:type,
+message:prof
+
+});
+}
+
+});
+router.post('/coursepage/bookcourse',function(req,res){
+
+StudentController.bookCourse(req,res,(err,book,type)=>{
+res.send(book);
+
+});
+
+
+});
+
+router.post('/studentprofile/review',function(req,res){
+if(req.decoded.type=="Student"){
+
+
+StudentController.typeReview(req,res,(err,review,type)=>{
+
+res.json({
+  type:type,
+  message:"Review added",
+  content:review
+});
+
+});
+}
+else {
+  res.json({
+    type:type,
+    message:review
+  });
+}
 
 
 
- router.post('/ServiceProvider/courses/removeCourse',function(req,res){
- 	console.log(re.decoded);
- 	return ServiceProviderController.removeCourse(req,res);
- });
+
+});
 
 
 
-    router.post('/ServiceProvider/courses/addCourse',function(req,res){
-    	console.log(req.decoded);
-    	return ServiceProviderController.addCourse(req,res);
-     
-    });
 
-  
+// router.post('/bookCourse', function(req,res){
+// // 	StudentController.bookCourse(req,res)
+// // });
 
-    router.post('/adminhomepage/viewunreg', function(req,res){
-    	return AdminController.viewUnregSP(req,res);
-    });
+router.post('/ServiceProvider/courses/addCourse',(req,res)=>{
+	ServiceProviderController.addCourse(req,res);
+});
 
-    
-    router.post('/adminhomepage/decline', function(req,res){
-       return AdminController.declineSP(req,res);
-    });
+// router.post('/ServiceProvider/courses/removeCourse',SPController.removeCourse);
 
-//  router.get('*',function (req,res){
-//         res.sendFile(path.join(__dirname,'../','app','index.html'))
-// })
+// router.post('/ServiceProvider/courses/postAnnouncment',SPController.postAnnouncements);
 
+// router.get('/Student/logout',(res,req)=>{
+//  	req.logout();
+// 	req.flash('success_msg','You are logged out');
+//  });
+
+// router.post('/updateCourse', SPController.updateCourse);
+
+// router.get('/serviceProvideRegister',(req,res)=>{
+// 	console.log('Service provider register page is required');
+// });
+// router.post('/ServiceProvideRegister',SPController.spRegister);
+// router.get('/viewUnRegSP',(req,res)=>{
+// 	console.log('view unregistered service provider page is required');
+// });
+
+// router.post('/viewUnRegSP',SPController.viewUnregSP);
+
+// router.get('/ServiceProviderLogin',(req,res)=>{
+// 	console.log('Service provider login');
+// });
+
+// router.post('/ServiceProviderLogin',SPController.SPLogin);
 
 module.exports =router;
+
 
 

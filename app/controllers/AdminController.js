0c541bf97dbb8eb921c913d3b928aac98880d88c
Roma-Yousef <romamohamed96.my@gmail.com>
2017-04-20 10:11:00 +0200

    semi fixed admin page

---
+++ app/controllers/AdminController.js
@@ -0,0 +1,58 @@
+angular.module('AdminController', []).config(function() {
+
+.controller('AdminController', function($scope,adminSrv,indexSrv,$location) {
+
+   //adminSrv.setEmail("balabizo@gmail.com");
+   $scope.msg = "";
+    $scope.test = "testemail";
+  //$scope.count = 0;
+
+  $scope.IsVisible = false;
+  $scope.ShowHide = function () {
+      //If DIV is visible it will be hidden and vice versa.
+       $scope.IsVisible = $scope.IsVisible ? false : true;
+
+  };
+
+  //  $scope.Email  = adminSrv.getEmail();
+  //removeSrvProvider($scope.Email);
+
+  $scope.SrvProviders = indexSrv.getAllVerifiedServiceProvider().success(function(Sp) {
+
+             indexSrv.setSP(Sp);
+
+             //removeSrvProviderconsole.log(Sp[0]);
+              $scope.SrvProviders  = Sp;
+              //console.log($scope.SrvProviders[0]);
+
+  });
+
+
+  $scope.removeSrvProvider  =function(Email){
+        console.log("ctrl");
+        console.log(Email);
+      adminSrv.removeSrvProvider(Email).success(function(msg) {
+
+             $scope.msg = msg;
+             console.log(msg);
+             console.log("ctrl2");
+             console.log(Email);
+
+             displaySrvProviders  ();
+         });
+  };
+
+  function  displaySrvProviders  (){
+      $scope.SrvProviders   ="";
+      $scope.SrvProviders   = indexSrv.getAllVerifiedServiceProvider().success(function(Sp) {
+
+            indexSrv.setSP(Sp);
+
+            //removeSrvProviderconsole.log(Sp[0]);
+             $scope.SrvProviders  = Sp;
+             //console.log($scope.SrvProviders[0]);
+
+          });
+        };
+
+  });
--- app/routes.js
+++ app/routes.js
@@ -1,87 +1,59 @@
 var myApp = angular.module('appRoutes',['ngRoute'])
 
 
-.config(function($routeProvider) {
+.config(function($routeProvider, $locationProvider) {
   $routeProvider
 
     .when('/', {
     templateUrl: 'views/welcome.html',
-    // controller: 'MainController',
-    // controllerAs: 'MainController'
                })
     .when('/catalog',{
-
     templateUrl:'/catalog.html'
-
     }).when('/welcome',{
 
     templateUrl:'views/welcome.html',
       controller: 'MainController',
     controllerAs: 'MainController'
-
     }).
    when('/search',{
-
     templateUrl:'views/search.html',
-   
-
     })
    .when('/registeredSP',{
-
     templateUrl:'views/registeredSp.html',
       controller: 'MainController',
     controllerAs: 'MainController'
-
     })
    .when('/register',{
-
     templateUrl:'views/register.html',
       controller: 'spCon',
     controllerAs: 'spCon'
-
     })
    .when('/test',{
-
     templateUrl:'views/test.html'
-    
-
     })
     .when('/login',{
-
     templateUrl:'views/login.html'
-    //   controller: 'MainController',
-    // controllerAs: 'MainController'
-
-    
-
     })
     .when('/logout',{
-
     templateUrl:'views/logout.html'
-    //   controller: 'MainController',
-    // controllerAs: 'MainController'
-
-    
-
     }).
    when('/search',{
-
     templateUrl:'views/search.html',
-   
-
     }).
     when('/signUp',{
-  
     templateUrl:'views/studentRegister.html',
      controller:'studentController',
      controllerAs:'studentController'
-
+    })
+    .when('/about',{
+      templateUrl:'views/about.html'
+    })
+    .when('/admin',{
+      templateUrl:'views/admin/admin.html'
     })
     .otherwise({
     redirectTo: "/"
-  });
-
-    
-  
-
+  })
+    $locationProvider.html5Mode({ enabled: true, requireBase: false });
+    // Required to remove AngularJS hash from URL (no base is required in index file);
 });

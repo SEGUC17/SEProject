38ce15a3f3b702b0cfd606c18691781a97fab778
habibahshm <habibahshm@gmail.com>
2017-04-21 22:56:35 +0200

    merged with dev

--- app/controllers/StudentController.js
+++ app/controllers/StudentController.js
@@ -8,16 +8,12 @@ angular.module('StudentController',['stServ'])
 
 // 	     $location.url('/signUp');
 // }
-<<<<<<< HEAD
-
-=======
 this.typeReview=(data)=>{
 	stServ.typeReview(this.data).then(function(argument) {
 		console.log(argument.data.content);
 	});
 
 }
->>>>>>> f0094daf23b98f0f2f216e3bf40e2383051888c0
   this.newStudent = function(data){
 
 		  stServ.studentRegister(this.data)

2f57c2810e3ccf4948ca6bcf5081f6688c44b61b
habibahshm <habibahshm@gmail.com>
2017-04-21 17:40:46 +0200

    finished multer

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -1,6 +1,7 @@
-angular.module('MainController', ['indexSrv', 'fileModelDirective','uploadFileService'])
+angular.module('MainController', ['indexSrv','uploadFileService'])
+//angular.module('MainController', ['indexSrv', 'fileModelDirective','uploadFileService'])
 
-.controller('MainController',function($scope,indexSrv,$location,$rootScope,uploadFile) {
+.controller('MainController',function($scope,indexSrv,$location,$rootScope,$timeout,uploadFile) {
 //console.log('main')
 
 indexSrv.getVerifiedServiceProvider().then(function(res){
@@ -24,6 +25,26 @@ indexSrv.getVerifiedServiceProvider().then(function(res){
         	$scope.file = {};
         }
  	});
+ }
+
+ $scope.photoChanged = function(files){
+    if(files.length > 0  && files[0].name.match(/\.(png|jpg|jpeg)$/)){
+         $scope.uploading = true;
+         var file = files[0];
+         var fileReader = new FileReader();
+         fileReader.readAsDataURL(file);
+         fileReader.onload = function(e){
+         	$timeout(function() {
+               $scope.thumbnail = {};
+               $scope.thumbnail.dataUrl = e.target.result;
+               $scope.uploading = false;
+               $scope.message =  false;
+         	});
+         }
+    }else{
+    	$scope.thumbnail ={};
+    	$scope.message = false;
+    }
  }
 
 
--- app/starter.js
+++ app/starter.js
@@ -1,5 +1,4 @@
 angular.module('myApp', ['MainController'])
 
 .config(function($httpProvider){
-  console.log("testing");
 });

b37dd7e8a5a4c64b6455b5ee66dc86b024bbdbd4
NadaBahaa <nada.bahiry@gmail.com>
2017-04-28 10:57:17 +0300

    admin page base project

---
+++ app/services/adminSrv.js
@@ -0,0 +1,109 @@
+angular.module('adminSrv', [])
+.factory('adminSrv', function($http ) {
+    return {
+
+// view all service provider
+getVerifiedServiceProvider: function(token){
+ // jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+
+ var body = {
+
+"token":token
+
+};
+    return $http.post('/home/viewreg',body);
+},
+
+// remove service provider
+declineSrvProvider: function(Email , token){
+       // jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+       var body = {
+      "email": Email ,
+      "token":token
+
+      }; console.log("declineSrvProvider");
+         console.log(Email);
+         return $http.post('/admin/declineSP',body);
+},
+getSpDetail: function(Email , token){
+       // jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+       var body = {
+      "email": Email ,
+      "token":token
+
+      };
+
+         return $http.post('/ServiceProvider/viewPortofolioByAdmin',body);
+},
+
+
+//verify SP
+VerifySrvProvider: function(username,password,email, token){
+       // jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+       var body = {
+      "username":username,
+      "password":password,
+      "email":email,
+      "token":token
+
+      }; console.log("VerifySrvProvider");
+         console.log(username);
+         console.log(password);
+         console.log("----------");
+         console.log(token);
+
+        return $http.post('/adminhomepage/verify',body);
+},
+
+
+
+// // delete sp
+// deleteSrvProvider: function(Email){
+//        // jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+//        var body = {
+//       "email": Email
+//
+//       }; console.log("deleteSrvProvider");
+//          console.log(Email);
+//          return $http.post('/admin/deleteSP',body);
+// },
+
+
+
+//view Unverify service provider
+viewUnSrvProvider: function(token){
+       // jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+  //   console.log("viewUnSrvProvider");
+    // console.log(username);
+      var body = {
+     "token":token
+
+     };
+      return $http.post('/adminhomepage/viewunreg',body);
+},
+
+getSP: function() {
+    return this.SP;
+},
+
+setSP: function(value) {
+    this.SP = value;
+},
+
+getEmail: function() {
+    return this.Email;
+},
+
+setEmail: function(value) {
+    this.Email = value;
+},
+getSpProfile: function() {
+    return this.SpProfile;
+},
+
+setSpProfile: function(value) {
+    this.SpProfile = value;
+}
+
+    };
+ });
---
+++ app/services/indexSrv.js
@@ -0,0 +1,152 @@
+angular.module('indexSrv', [])
+
+myApp.factory('indexSrv', function($http,AuthToken) {
+
+
+  var savedData={}
+    return {
+       postReview:function(data){
+        return $http.post('/studentprofile/review',data).then(function(response){
+         console.log('get revieww ' )
+         console.log(response)
+            return response;
+           });
+
+          },
+
+     set:function(data){
+      savedData=data;
+     },
+    get : function(){
+       return savedData;
+    },
+
+    	 getStudentProfile:function(data){
+              return $http.post('/studentprofile',data).then(function(res){
+                console.log(res)
+                return res;
+              });
+
+       },
+         getCatalog : function(){
+                  return $http.get('/catalog');
+          },
+       //      getStudentProfile:function(){
+       //        return $http.get('/studentprofile');
+
+       // },
+
+
+
+
+   Search:function(data){
+        return $http.post('/home/search',data);
+     },
+
+
+// THIS IS THE ROUTE OF SPLOGIN , AUTHTOKEN.SETTOKEN TAKE THE USER TOKEN AND SAVES IT IN THE LOCALSTRATEGY SO THAT
+// ANY FUNCTION WHICH NEEDS TOKEN AS INPUT WILL BE AUTOMATICALY TAKEN FROM HERE
+
+       ServiceProviderLogin:function(data){
+       return $http.post('/forbussinus/login',data).then(function(response){
+          AuthToken.SetToken(response.data.token)
+         //console.log(response)
+            return response;
+       });
+
+       },
+
+
+       StudentLogin:function(data){
+       return $http.post('/login',data).then(function(response){
+          AuthToken.SetToken(response.data.token)
+         //console.log(response)
+            return response;
+       });
+
+       },
+
+
+
+// function that return true if the user is logged in or return false when user is not logged in
+// it uses the function getToken() to check if there is a token , if there is token , the function will return true
+// if there is no token the function will return false
+       IsLoggedIn: function(){
+
+       if(AuthToken.GetToken()){
+         return true;
+       }else{
+         return false;
+       }
+
+      },
+
+      LogOut: function(){
+        AuthToken.SetToken();
+        console.log("logout from indexSrv")
+      },
+
+
+// function which return the current user
+      GetCurrentUser:function(){
+        if(AuthToken.GetToken()){
+          console.log('i have token')
+          return $http.post('/me');
+        }else
+        {
+          $q.reject({message:"user has no token"})
+        }
+      },
+      GetToken:function(){
+      return AuthToken.GetToken();
+
+    },
+
+
+
+
+
+
+    }
+})
+
+
+.factory('AuthToken',function($window){
+ return{
+
+// set token is  a function which takes parameter token and set  it to the localSTorage of the user
+   SetToken : function(token){
+    if(token){
+
+     $window.localStorage.setItem('token',token)
+    }else{
+     $window.localStorage.removeItem('token')
+
+    }
+
+
+
+   },
+   // function which retrieves the token from the localStorage
+   GetToken: function(){
+     return $window.localStorage.getItem('token')
+   },
+
+ }
+
+})
+
+.factory('AuthInterceptors',function(AuthToken){
+  return{
+
+    request: function(config){
+      var token= AuthToken.GetToken();
+      if(token)
+        config.headers['x-access-token']= token;
+      return config;
+
+    }
+
+
+  }
+})
